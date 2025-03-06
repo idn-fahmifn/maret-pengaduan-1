2f84777d9aa412e442390339630fbd8b7adba099
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-03-06 10:12:23 +0700

    4.2 table

--- resources/views/user/laporan/index.blade.php
+++ resources/views/user/laporan/index.blade.php
@@ -12,23 +12,41 @@
 
                     {{-- judul pada card halaman --}}
                     <h4 class="text-lg font-semibold dark:text-gray-200 text-gray-700">Laporan Saya</h4>
-                    <span class="text-sm dark:text-gray-200 text-gray-700">Semua progress dan laporan saya, klik pada judul untuk melihat detail</span>
+                    <span class="text-sm dark:text-gray-200 text-gray-700">Semua progress dan laporan saya, klik pada
+                        judul untuk melihat detail</span>
 
                     {{-- form search dan button new laporan --}}
-
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
                         {{-- search area --}}
 
                         <div class="">
                             <form action="#" method="get">
                                 <div class="flex items-center">
-                                    <input type="text" name="keyword" id="keyword" value="{{ Request::get('keyword') }}" placeholder="Cari laporan anda disini"
-                                    class="rounded-l-md py-2 px-4 bg-transparent dark:text-gray-200 w-full focus:outline-none">
-                                    <button type="submit" class="bg-red-500 border border-red-500 text-white rounded-r-md py-2 px-4">Cari</button>
+                                    <input type="text" name="keyword" id="keyword" value="{{ Request::get('keyword') }}"
+                                        placeholder="Cari laporan anda disini"
+                                        class="rounded-l-md py-2 px-4 bg-transparent dark:text-gray-200 w-1/2 focus:outline-none">
+                                    <button type="submit"
+                                        class="bg-red-500 border border-red-500 hover:bg-red-600 text-white rounded-r-md py-2 px-4">Cari</button>
                                 </div>
                             </form>
                         </div>
+                        <div class="text-start sm:text-end md:text-end lg:text-end">
+                            <a href="#"
+                                class="text-md font-semibold bg-red-500 hover:bg-red-600 text-gray-200 py-2 px-4 rounded-md">Buat
+                                Laporan</a>
+                        </div>
+                    </div>
 
+                    {{-- Area tabel --}}
+
+                    <div class="overflow-x-auto mt-4">
+                        <table class="min-w-full bg-gray-200 dark:bg-transparent py-2">
+                            <thead class="border border-x-0 border-gray-500">
+                                <th class="py-2 px-4 text-gray-500 dark:text-gray-200 uppercase">judul laporan</th>
+                                <th class="py-2 px-4 text-gray-500 dark:text-gray-200 uppercase">status</th>
+                                <th class="py-2 px-4 text-gray-500 dark:text-gray-200 uppercase">dibuat</th>
+                            </thead>
+                        </table>
                     </div>
 
                 </div>
